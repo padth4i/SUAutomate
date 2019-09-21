29c1f7e69c849487e7b947a20ad53934fd35702f
Vaishnav Sivaprasad <vaishnavs4201@gmail.com>
2019-09-21 15:45:35 +0530

    create reminder bot and update email bot

--- reminderbot.py
+++ reminderbot.py
@@ -5,45 +5,31 @@ import smtplib
 TOKEN = "975334004:AAEVotz_4_bsfHY_lGc1AzMDwhySgqBCFDw"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-sender_id = "senders_email_id"
-sender_password = "senders_password"
-thread_id = "recievers_email_id"
-s.login(sender_id, sender_password)
-
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
-
 
 def get_json_from_url(url):
     content = get_url(url)
     js = json.loads(content)
     return js
 
-
 def get_updates():
     url = URL + "getUpdates"
     js = get_json_from_url(url)
     return js
 
-
-def get_last_chat_id_and_text(updates):
+def get_last_chat_id(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
-
+    return chat_id
 
 def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
-    message = text
-    s.sendmail(sender_id, thread_id, message)
 
-
-text, chat = get_last_chat_id_and_text(get_updates())
+text = "Good evening! Don't forget to push today's status update.\nUse the following format: \n\n\tSUBJECT: Status Update [DD-MM-YYYY]\n\tWork Done: *****\n\tPlanned: *****\n\tBlockers: *****\n\nRemember to send detailed updates about your progress today. Thank you!"
+chat = get_last_chat_id(get_updates())
 send_message(text, chat)
